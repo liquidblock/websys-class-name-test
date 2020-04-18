0f69da53ef8ca787149a12c818682a9e885f8e50
liquidblock <liquidblock@users.noreply.github.com>
2020-04-18 23:43:37 +0200

    Add simple test case for Element::class_name and Element::set_class_name for SVG Elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+pub const SVG_NAMESPACE: &str = "http://www.w3.org/2000/svg";
+
+pub fn test() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    let svg: web_sys::Element = document.create_element_ns(Some(SVG_NAMESPACE), "svg")?;
+
+    body.append_child(&svg)?;
+
+    console::log_1(&"Test Begin".into());
+    svg.set_class_name("test");
+    let _: String = svg.class_name();
+    console::log_1(&"Test End".into());
+
+    Ok(())
+}
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    use console_error_panic_hook;
+    use std::panic;
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    test().expect("test returned error result");
+    panic!("this is a test, if the panic hook works correctly")
 }
